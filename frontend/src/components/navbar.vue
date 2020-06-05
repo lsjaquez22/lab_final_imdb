bbe1dd8267bf1d41b9ce873aa4f2fc3e0606f32e
Luis Sanchez <lsjaquez22@gmail.com>
2020-06-05 01:05:12 -0600

    change :to in logo

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -70,9 +70,9 @@
         aria-label="main navigation"
       >
         <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
+          <router-link class="navbar-item" to="/">
             <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-          </a>
+          </router-link>
 
           <a
             role="button"
